f4cec581b5e84207bf323d9c9555ff08cfe2b558
lAntraXxXl <79475476+lAntraXxXl@users.noreply.github.com>
2021-03-02 15:17:39 -0600

    Updates

---
+++ precache-manifest.f96873d0e6bebc2fed0f31294bf931b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75eaea184f9eed68e6702dfd53d0f220",
+    "url": "/Presupuesto_personal/index.html"
+  },
+  {
+    "revision": "8295d9f76212c9a91d18",
+    "url": "/Presupuesto_personal/static/css/main.d829634a.chunk.css"
+  },
+  {
+    "revision": "c8e80736c94208e42751",
+    "url": "/Presupuesto_personal/static/js/2.dbb6fd16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Presupuesto_personal/static/js/2.dbb6fd16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8295d9f76212c9a91d18",
+    "url": "/Presupuesto_personal/static/js/main.daeba85b.chunk.js"
+  },
+  {
+    "revision": "18f7e60b1265718e7a9b",
+    "url": "/Presupuesto_personal/static/js/runtime-main.4649a338.js"
+  }
+]);
